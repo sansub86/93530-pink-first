e84391a39fa5c834225a98c98a2f87c5f6cf4070
sansub86 <sun_mail_86@mail.ru>
2016-10-20 01:08:45 +1000

    Исправление страницы фотографий

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var burgerIcon = document.querySelector(".main-nav__burger");
 var crossIcon = document.querySelector(".main-nav__cross");
 var mainMenu = document.querySelector(".main-menu");
 var mainNav = document.querySelector(".main-nav");
+btnSwitch.classList.remove("main-nav__switch--cross");
+burgerIcon.classList.remove("main-nav--hide");
+crossIcon.classList.add("main-nav--hide");
+mainMenu.classList.add("main-menu--closed");
+mainNav.classList.remove("main-nav--opacity");
 btnSwitch.addEventListener ("click", function(event) {
   event.preventDefault();
   btnSwitch.classList.toggle("main-nav__switch--cross");
